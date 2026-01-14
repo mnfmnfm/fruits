55a1adfea69fbe837eee4cb017ffc5df69f6c3ad
Michelle Ferreirae <mnfmnfm@gmail.com>
2026-01-14 08:26:20 -0500

    trap the user in a loop until they name a good fruit

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ fruits = ["apple", "banana", "cantaloupe", "dragonfruit"]
 for fruit in fruits:
     print(fruit)
 
-user_fruit = input("what is your favorite fruit?\n")
-if user_fruit in fruits:
-    print("good job, that is a fruit")
-else:
-    print("sorry, that is not a fruit, there are only 4 fruits: " + ", ".join(fruits))
+while True:
+    user_fruit = input("what is your favorite fruit?\n")
+    if user_fruit in fruits:
+        print("good job, that is a fruit")
+        break
+    else:
+        print("sorry, that is not a fruit, there are only 4 fruits: " + ", ".join(fruits))
+        print("please try again")
